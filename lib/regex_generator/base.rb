f46dd17066b1babb78e677b3084dc1dd39e40380
arye-dov-eidelman <aryedov.e@gmail.com>
2018-08-12 16:56:43 -0400

    initialize and random regex generater

--- lib/regex_generator/base.rb
+++ lib/regex_generator/base.rb
@@ -1,2 +1,30 @@
 class RegexGenerator::Base
+  attr_accessor :true_test_casses, :false_test_casses, :top_results, :final_result, :allowed_regex_chars, :randomizer, :max_length
+
+
+  def initialize(setup_hash)
+    @allowed_regex_chars = ['\d', '\w', '\s', '\D', '\W', '\S', '+', '*', '?', '.', '\.', '\?', '\*', '\+']
+    @randomizer = Random.new
+    
+    @max_length = setup_hash[:max_length] || 8
+    @true_test_casses = setup_hash[:true_test_casses] || []
+    @false_test_casses = setup_hash[:false_test_casses] || []
+  end
+
+  def random_char
+    [
+      allowed_regex_chars.sample,
+      ('a'..'z').to_a.sample,
+      ('A'..'Z').to_a.sample,
+      ('0'..'9').to_a.sample
+    ].sample
+  end
+  
+  def random_regex(length = randomizer.rand(1..max_length))
+    length.times.collect{random_char}.join
+  end
+
+  def generate
+
+  end
 end
